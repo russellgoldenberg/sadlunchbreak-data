c10afc8d9de05c4d922087f970e3aef7ce1a1f56
russellgoldenberg <russellgoldenberg@gmail.com>
2019-10-10 08:08:29 -0400

    use datas3

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const request = require('request');
 const d3 = require('d3');
-const uploadToS3 = require('./upload-to-s3');
-const AWS_PATH = 'misc/sadlunchbreak';
+const dataS3 = require('data-s3');
+
+const SHEET_OPTS = {
+	id: '14q0-XFUKBGJSbUjkn79haheJSQ5EdCN1vt_fOI79COI',
+	gid: '589998460'
+};
 
 function downloadSheet({ id, gid }) {
   return new Promise((resolve, reject) => {
@@ -16,25 +20,34 @@ function downloadSheet({ id, gid }) {
   });
 }
 
+function parse(response) {
+	const approved = response.filter(d => d.approved);
+	approved.sort((a, b) => d3.descending(new Date(a.Timestamp), new Date(b.Timestamp)));
+	const top = approved.slice(0, 100);
+	const timestamp = Date.now();
+	const data = { timestamp, data: top };
+
+	return Promise.resolve(data);
+}
+
+async function upload(data) {
+	const accessKeyId = process.env.AWS_KEY;
+	const secretAccessKey = process.env.AWS_SECRET;
+	const region = 'us-east-1';
+	dataS3.init({ accessKeyId, secretAccessKey, region });
+
+	const bucket = process.env.AWS_BUCKET;
+	const path = 'misc/sadlunchbreak';
+	const file = 'data.json';
+	await dataS3.upload({ bucket, path, file, data });
+	process.exit();
+}
+
 function init() {
-  downloadSheet({
-    id: '14q0-XFUKBGJSbUjkn79haheJSQ5EdCN1vt_fOI79COI',
-    gid: '589998460'
-  }).then(response => {
-    const approved = response.filter(d => d.approved);
-		approved.sort((a, b) => d3.descending(new Date(a.Timestamp)), new Date(b.Timestamp));
-    const top = approved.slice(0, 100);
-		// const bottom = approved.slice(100, 300);
-    const timestamp = Date.now();
-    const output = { timestamp, data: top };
-    const string = JSON.stringify(output, null, 2);
-    const path = `${AWS_PATH}/data`;
-    uploadToS3({ string, path, ext: 'json' })
-      .then(() => {
-        process.exit();
-      })
-      .catch(console.log);
-  });
+	downloadSheet(SHEET_OPTS)
+		.then(parse)
+		.then(upload)
+		.catch(console.error);
 }
 
 init();
